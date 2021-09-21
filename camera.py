ec60de9167d03880649e9b409b755d4a9faeac5c
James Kulu <jameskulu55@gmail.com>
2021-09-21 19:34:49 +0600

    Added authorized person detect and buzzer functionality

--- camera.py
+++ camera.py
@@ -3,6 +3,17 @@ from imutils.video.pivideostream import PiVideoStream
 import imutils
 import time
 import numpy as np
+import pickle
+
+
+with open('labels', 'rb') as f:
+    dicti = pickle.load(f)
+    f.close()
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml") 
+font = cv2.FONT_HERSHEY_SIMPLEX
+
 
 class VideoCamera(object):
     def __init__(self, flip = False):
@@ -24,8 +35,10 @@ class VideoCamera(object):
         return jpeg.tobytes()
 
     def get_object(self, classifier):
+        
         found_objects = False
         frame = self.flip_if_needed(self.vs.read()).copy() 
+        ret, jpeg = cv2.imencode('.jpg', frame)
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
         objects = classifier.detectMultiScale(
@@ -36,14 +49,33 @@ class VideoCamera(object):
             flags=cv2.CASCADE_SCALE_IMAGE
         )
 
-        if len(objects) > 0:
-            found_objects = True
-
-        # Draw a rectangle around the objects
         for (x, y, w, h) in objects:
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roiGray = gray[y:y+h, x:x+w]
 
-        ret, jpeg = cv2.imencode('.jpg', frame)
+            id_, conf = recognizer.predict(roiGray)
+
+            for name, value in dicti.items():
+                if value == id_:
+                    print("-----------------------------")
+
+            if conf <= 70:
+                found_objects = False
+
+                # Draw a rectangle around the objects
+                for (x, y, w, h) in objects:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                return (jpeg.tobytes(), found_objects)
+            else:
+                print("Unauthorized person detected !!!!")
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                found_objects = True
+                return (jpeg.tobytes(), found_objects)
         return (jpeg.tobytes(), found_objects)
+
+
+
+        
 
 
--- main.py
+++ main.py
@@ -6,10 +6,14 @@ from camera import VideoCamera
 from flask_basicauth import BasicAuth
 import time
 import threading
+import RPi.GPIO as GPIO
+from time import sleep
 
-email_update_interval = 120 # sends an email only once in this time interval
-video_camera = VideoCamera(flip=False) # creates a camera object, flip vertically
-object_classifier = cv2.CascadeClassifier("./models/facial_recognition_model.xml") # an opencv classifier
+email_update_interval = 120  # sends an email only once in this time interval
+# creates a camera object, flip vertically
+video_camera = VideoCamera(flip=False)
+object_classifier = cv2.CascadeClassifier(
+    "models/facial_recognition_model.xml")  # an opencv classifier
 
 # App Globals
 app = Flask(__name__)
@@ -20,23 +24,39 @@ app.config['BASIC_AUTH_FORCE'] = True
 basic_auth = BasicAuth(app)
 last_epoch = 0
 
+
+buzzerPIN = 17
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(buzzerPIN, GPIO.OUT)
+
+
 def check_for_objects():
-	global last_epoch
-	while True:
-		try:
-			frame, found_obj = video_camera.get_object(object_classifier)
-			if found_obj and (time.time() - last_epoch) > email_update_interval:
-				last_epoch = time.time()
-				print ("Sending email...")
-				sendEmail(frame)
-				print ("done!")
-		except:
-			print ("Error sending email: "), sys.exc_info()[0]
+    global last_epoch
+    while True:
+        try:
+            frame, found_obj = video_camera.get_object(object_classifier)
+            if found_obj and (time.time() - last_epoch) > email_update_interval:
+                last_epoch = time.time()
+                for i in range(3):
+                    GPIO.output(buzzerPIN, GPIO.HIGH)
+                    sleep(0.5)
+                    GPIO.output(buzzerPIN, GPIO.LOW)
+                    sleep(0.5)
+                print("Sending email...")
+                sendEmail(frame)
+                print("done!")
+        except Exception as e:
+            print(e)
+
 
 @app.route('/')
 @basic_auth.required
 def index():
     return render_template('index.html')
+
 
 def gen(camera):
     while True:
@@ -44,13 +64,15 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(video_camera),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
     t = threading.Thread(target=check_for_objects, args=())
     t.daemon = True
     t.start()
-    app.run(host='0.0.0.0',debug=False)
+    app.run(host='0.0.0.0', debug=False)
